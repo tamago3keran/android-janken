7e136329470414911e0fdca62d51ed6b00fd0f2c
tamago3keran <yamataku3831@gmail.com>
2020-06-14 15:03:53 +0900

    create logic

--- app/src/main/java/com/example/janken/MainActivity.kt
+++ app/src/main/java/com/example/janken/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.janken
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rock.setOnClickListener { onJankenButtonTapped(it) }
+        scissors.setOnClickListener { onJankenButtonTapped(it) }
+        paper.setOnClickListener { onJankenButtonTapped(it) }
+    }
+
+    fun onJankenButtonTapped(view: View?) {
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("MY_HAND", view?.id)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/janken/ResultActivity.kt
+++ app/src/main/java/com/example/janken/ResultActivity.kt
@@ -2,10 +2,49 @@ package com.example.janken
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
+
+    val rock = 0
+    val scissors = 1
+    val paper = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        val id = intent.getIntExtra("MY_HAND", 0)
+
+        val myHand: Int
+        myHand = when(id) {
+            R.id.rock -> {
+                myHandImage.setImageResource(R.drawable.human_rock)
+                rock
+            }
+            R.id.scissors -> {
+                myHandImage.setImageResource(R.drawable.human_scissors)
+                scissors
+            }
+            R.id.paper -> {
+                myHandImage.setImageResource(R.drawable.human_paper)
+                paper
+            }
+            else -> rock
+        }
+
+        val comHand = (Math.random() * 3).toInt()
+        when(comHand) {
+            rock -> comHandImage.setImageResource(R.drawable.machine_rock)
+            scissors -> comHandImage.setImageResource(R.drawable.machine_scissors)
+            paper -> comHandImage.setImageResource(R.drawable.machine_paper)
+        }
+
+        val gameResult = (comHand - myHand + 3) % 3
+        when(gameResult) {
+            0 -> resultLabel.setText(R.string.result_draw)
+            1 -> resultLabel.setText(R.string.result_win)
+            2 -> resultLabel.setText(R.string.result_lose)
+        }
+        backButton.setOnClickListener { finish() }
     }
 }
